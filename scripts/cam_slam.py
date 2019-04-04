ad7e6ce5011ef048a902447582e66678fcdbcd3a
Ray Zayas <rayzayas@udel.edu>
2019-04-04 15:16:12 -0400

    Commit before main slam overhaul

--- scripts/cam_slam.py
+++ scripts/cam_slam.py
@@ -11,6 +11,7 @@ from geometry_msgs.msg import *
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CameraInfo
 from fiducial_msgs.msg import *
+from navigation_stack.srv import *
 from tf import transformations as t
 
 class CamSlam:
@@ -21,46 +22,114 @@ class CamSlam:
 		topic = "camera_%s/fiducial_transforms" % cam_number
 
 		rospy.init_node("cam_slam_%s" % cam_number, anonymous=False, log_level=rospy.DEBUG)
-		self.sub = rospy.Subscriber("camera_1/fiducial_transforms", FiducialTransformArray, self.updateCamLocation)
+		self.sub = rospy.Subscriber("camera_1/fiducial_transforms", FiducialTransformArray, self.updateBuffer, queue_size=1)
 		self.rate = rospy.Rate(60) # in hz
 
+		self.tag_log_service = rospy.ServiceProxy('tag_log',TagLog)
+		self.tag_lookup_service = rospy.ServiceProxy('tag_lookup',TagLookup)
+
 		##Localization var
-		self.cam_position = []
-		self.tag_buffer = None
+		self.origin_set = False
+		self.cam_position = None
+		self.tag_array_tmp = None
 		self.setup_mode = setup_mode
+		self.tag_known_buffer = []
+		self.tag_unknown_buffer = []
+		self.new_tag_buffer = []
 
 		##Loop
 		while not rospy.is_shutdown():
 			self.main()
 			self.rate.sleep()
 
-	def main():
+	def main(self):
 		""" Main Loop """
 
 		#Grab recent tags detected
-		fiducial_array = self.
+		fiducial_array = self.tag_array_tmp
+
+		if fiducial_array is None:
+			return
 
 		if self.setup_mode:
 			self.setup_loop(fiducial_array)
 		else:
 			self.nav_loop(fiducial_array)
 
-	def nav_loop(fiducial_array):
+	def nav_loop(self, fiducial_array):
 		""" Loop for navigation at runtime """
 		print "unfinished"
 
-	def setup_loop(fiducial_array):
+	def origin_loop(self, fiducial_array):
+		""" Loop for setting origin """
+
+		origin_tags = [0,1,2,3]
+		origin_tags_info = [None,None,None,None]
+		tag_count = 0
+		for tag in fiducial_array:
+			if tag.fiducial_id in origin_tags:
+				tag_count = tag_count + 1
+				origin_tags_info[tag.fiducial_id] = tag.transform
+			if tag_count == 4:
+				rospy.loginfo("Setting Origin...")
+				self.tag_log_service(0,Transform())
+				self.origin_set = True
+
+		print tag_count
+
+	def setup_loop(self, fiducial_array, tracking_threshold=1, new_tag_threshold=10):
 		""" Loop for map generation """
 
+		if not self.origin_set:
+			rospy.loginfo("Origin Setup: Focus Tags 0-3")
+			self.origin_loop(fiducial_array)
+			return
+
+		# Sort based on whether it's in database
 		for tag in fiducial_array:
-			print tag
+			try:
+				response = self.tag_lookup_service(tag.fiducial_id)
+				if response.id != -1: #In database
+					self.tag_known_buffer.append(response)
+				else:
+					self.tag_unknown_buffer.append(tag)
+			except rospy.ServiceException, e:
+				print "Service call failed: %s"%e
+
+		# Localize based on known tags
+		if len(self.tag_known_buffer) >= tracking_threshold:
+			tracking = True
+		else:
+			tracking = False
+		localize_tmp = []
+		#TEMP: This needs to be fixed and moved to sthe main slam node
+		for tag in self.tag_known_buffer:
+			localize_tmp.append(inverseTransform(tag.transform))
+		if tracking:
+			sum(localize_tmp)/len(localize_tmp)
+
+		# Localize unknown tags
+		if tracking:
+			for tag in self.tag_unknown_buffer:
+				tag_position = addTransforms(self.cam_position, inverseTransform(tag.transform))
+				if self.new_tag_buffer[0] == None:
+					self.new_tag_buffer.append(tag.id)
+				if self.new_tag_buffer[0] == tag.id:
+					self.new_tag_buffer.append(tag_position)
+					if len(self.new_tag_buffer) >= new_tag_threshold:
+						self.tag_log_service(tag.fiducial_id, tag_position)
+						self.new_tag_buffer = []
+
+		# Reset buffer
+		self.tag_known_buffer = []
+		self.tag_unknown_buffer = []
 
 	def tag_log(self):
-		"""Retrieve a tag's world position"""
+		"""Log a tag's position into the database"""
 		print "unfinished"
 
 	def tag_lookup(self):
-		"""Log a tag's position into the database"""
+		"""Retrieve a tag's world position, returns None if nothing found"""
 		print "unfinished"
 
 	def updateBuffer(self, msg):
@@ -68,7 +137,62 @@ class CamSlam:
 
 		#TODO: Mutex Lock
 		if msg.transforms:
-			self.tag_buffer = msg
+			self.tag_array_tmp = msg.transforms
+
+#####################TEMP#######################################################
+def addTransforms(frame1, frame2):
+	""" Adds two transform objects to get a resulting transform. """
+
+	t1 = np.array([frame1.translation.x,frame1.translation.y,frame1.translation.z])
+	t2 = np.array([frame2.translation.x,frame2.translation.y,frame2.translation.z])
+
+	r1 = np.array([frame1.rotation.x, frame1.rotation.y, frame1.rotation.z, frame1.rotation.w])
+	r2 = np.array([frame2.rotation.x, frame2.rotation.y, frame2.rotation.z, frame2.rotation.w])
+
+	f1 = t.compose_matrix(translate=t1,angles=t.euler_from_quaternion(r1))
+	f2 = t.compose_matrix(translate=t2,angles=t.euler_from_quaternion(r2))
+
+	f = np.matmul(f1,f2)
+
+	trans = t.translation_from_matrix(f)
+	rot = t.quaternion_from_matrix(f)
+
+	trans = Vector3(trans[0],trans[1],trans[2])
+	rot = Quaternion(rot[0],rot[1],rot[2],rot[3])
+
+	return Transform(trans, rot)
+
+def arrayify(n):
+	""" Turn the transform into two nparrays, translation and rotation """
+
+	trans = np.array([n.translation.x, n.translation.y, n.translation.z])
+	rot = np.array([ n.rotation.x, n.rotation.y, n.rotation.z, n.rotation.w])
+
+	return trans, rot
+
+def inverseTransform(input_transform):
+
+	translation, rotation = arrayify(input_transform)
+
+	transform = t.compose_matrix(translate=translation,angles=t.euler_from_quaternion(rotation))
+	inversed_transform = t.inverse_matrix(transform)
+
+	translation = t.translation_from_matrix(inversed_transform)
+	quaternion = t.quaternion_from_matrix(inversed_transform)
+
+	final_transform = transformify(translation, quaternion)
+
+	return final_transform
+
+def transformify(t, r):
+	""" Turns a translation and rotation to a transform object """
+
+	translation = Vector3(t[0],t[1],t[2])
+	rotation= Quaternion(r[0],r[1],r[2],r[3])
+	transform = Transform(translation, rotation)
+
+	return transform
+################################################################################
 
 if __name__ == "__main__":
 	cam_slam = CamSlam(1,True)
--- scripts/tag_handler.py
+++ scripts/tag_handler.py
@@ -7,17 +7,55 @@ import yaml
 import sys
 import rospy
 import numpy as np
-from geometry_msgs.msg import *
-from sensor_msgs.msg import Image
-from sensor_msgs.msg import CameraInfo
+import pyavltree
+import os
 from fiducial_msgs.msg import *
+from geometry_msgs.msg import *
+from navigation_stack.srv import *
 from tf import transformations as t
 
 class tag_handler:
 
 	def __init__(self):
-		print "under construction"
 
-		self.id_db[] = {}
+		##ROS
+		rospy.init_node("tag_database", anonymous=True, log_level=rospy.DEBUG)
+		self.service_log = rospy.Service('tag_log', TagLog, self.tag_log)
+		self.service_lookup = rospy.Service('tag_lookup', TagLookup, self.tag_lookup)
 
-	def
+		##Tag
+		self.tag_tree = pyavltree.AVLTree()
+
+	def database_load(self):
+		"""Load database from file"""
+		rospy.logdebug("Loading tag configuration")
+		#TEMP: Temporary file location, link dynamically
+		with open('id_db.yml', 'r') as yaml_file:
+			self.tag_tree = yaml.load(yaml_file)
+
+	def database_save(self):
+		"""Saves database to file"""
+		rospy.loginfo("Saving tag configuration..")
+		#TEMP: Temporary file location, link dynamically
+		with open('id_db.yml', 'w') as yaml_file:
+			yaml.dump(self.tag_tree, yaml_file)
+
+	def tag_log(self, msg):
+		"""Store tag info in database"""
+		self.tag_tree.insert(msg.id, msg.transform)
+		return True
+
+	def tag_lookup(self, msg):
+		"""Retrieve tag info in database, returns id of -1 if false"""
+		try:
+			transform = self.tag_tree.find(msg.id).data
+			return TagLookupResponse(msg.id, transform)
+		except:
+			return TagLookupResponse(-1, Transform())
+
+if __name__ == "__main__":
+	test = tag_handler()
+	setup_mode = True
+	if setup_mode == True:
+		atexit.register(test.database_save)
+	rospy.spin()
